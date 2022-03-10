bc4e630c7d1231ff1bfd7cb2578a5ba32a8f0431
Lee Seung Yun <ileilliat@gmail.com>
2022-03-10 16:38:15 +0900

    Add menu command option

--- src/extension.ts
+++ src/extension.ts
@@ -10,28 +10,29 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 	);
 
-	const menu = vscode.commands.registerCommand('ko-en-conversion.conversion', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			const document = editor.document;
-			const selection = editor.selection;
-			const [range, convWord] = conversion.convert(document, selection);
-			editor.edit(editBuilder => {
-				editBuilder.replace(range, convWord);
-			});
-
-		} else {
-			vscode.window.showWarningMessage('한영 변환을 수행할 부분을 선택한 후 명령을 실행하십시오.');
-		}
+	const conversionAll = vscode.commands.registerCommand('ko-en-conversion.conversion', () => {
+		conversion.edit(Lang.all);
+	});
+	const conversionKo = vscode.commands.registerCommand('ko-en-conversion.english', () => {
+		conversion.edit(Lang.en);
+	});
+	const conversionEn = vscode.commands.registerCommand('ko-en-conversion.korean', () => {
+		conversion.edit(Lang.ko);
 	});
 
-	context.subscriptions.push(menu);
+	context.subscriptions.push(conversionAll);
+	context.subscriptions.push(conversionKo);
+	context.subscriptions.push(conversionEn);
+}
+
+export enum Lang {
+	all, ko, en
 }
 
 export class Conversion {
 	private map: Map<string, string> = unicode.getKoMap();
 
-	public convert(document: vscode.TextDocument, range: vscode.Range | vscode.Selection): [vscode.Range, string] {
+	public convert(document: vscode.TextDocument, range: vscode.Range | vscode.Selection, lang: Lang = Lang.all): [vscode.Range, string] {
 		let first = range.start.character;
 		let last = range.end.character;
 		let text = document.lineAt(range.start.line).text;
@@ -59,8 +60,23 @@ export class Conversion {
 
 
 		// 단어 변환
-		const convWord = this.conversionWord(word);
+		const convWord = this.conversionWord(word, lang);
 		return [wordRange, convWord];
+	}
+
+	public edit(lang: Lang) {
+		const editor = vscode.window.activeTextEditor;
+		if (editor) {
+			const document = editor.document;
+			const selection = editor.selection;
+			const [range, convWord] = conversion.convert(document, selection, lang);
+			editor.edit(editBuilder => {
+				editBuilder.replace(range, convWord);
+			});
+
+		} else {
+			vscode.window.showWarningMessage('한영 변환을 수행할 부분을 선택한 후 명령을 실행하십시오.');
+		}
 	}
 
 	// return selected word, last
@@ -77,12 +93,12 @@ export class Conversion {
 		return [first + 1, last];
 	}
 
-	private conversionWord(word: string): string {
+	private conversionWord(word: string, lang: Lang): string {
 		let cText = "";
 		for (const c of word) {
 			const code = c.charCodeAt(0);
 
-			if (0xAC00 <= code && code <= 0xD7AF) { // 한글 글자마디
+			if (lang !== Lang.en && 0xAC00 <= code && code <= 0xD7AF) { // 한글 글자마디
 				// 초중종성 분리
 				const idx = code - 0xAC00;
 				const top = Math.floor(Math.floor(idx / 28) / 21) + 0x1100;
@@ -95,16 +111,20 @@ export class Conversion {
 				} else {
 					cText += `${this.map.get(String.fromCharCode(top))}${this.map.get(String.fromCharCode(mid))}${this.map.get(String.fromCharCode(bot))}`;
 				}
-			} else if (65 <= code && code <= 90 || 97 <= code && code <= 122) { // 영어 알파벳
-				//cText += String.fromCharCode(this.map.get(c)?.charCodeAt(0)!!);
+			} else if(lang !== Lang.en && ((0x1100 <= code && code <=0x11FF) || (0x3131 <= code && code <= 0x318E))) { // 한글 자음, 모음
+				cText += (this.map.get(c) ?? c);
+			} else if (lang !== Lang.ko && 65 <= code && code <= 90 || 97 <= code && code <= 122) { // 영어 알파벳
 				cText += c;
 				vscode.window.showWarningMessage('영어 -> 한글 변환을 아직 지원하지 않습니다.');
 			} else { // 이외 문자
-				cText += (this.map.get(c) ?? c);
+				// 기존 문자 그대로 유지
+				cText += c;
 			}
 		}
 		return cText;
 	}
+
+
 }
 const conversion = new Conversion();
 
